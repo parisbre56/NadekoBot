e2667eaf3a68b88657163860a4c40b4abf27b365
parisbre56 <parisbre56@users.noreply.github.com>
2018-10-16 12:05:20 +0300

    added deadly wheel

--- NadekoBot.Core/Modules/Gambling/WheelOfFortuneCommands.cs
+++ NadekoBot.Core/Modules/Gambling/WheelOfFortuneCommands.cs
@@ -20,16 +20,25 @@ namespace NadekoBot.Modules.Gambling
     {
         public class WheelOfFortuneCommands : GamblingSubmodule<GamblingService>
         {
-            private static readonly ImmutableArray<string> _emojis = new string[] {
-            "⬆",
-            "↖",
-            "⬅",
-            "↙",
-            "⬇",
-            "↘",
-            "➡",
-            "↗" }.ToImmutableArray();
+            private static readonly ImmutableArray<Tuple<decimal, string>> _results = new Tuple<decimal, string>[] {
+            Tuple.Create(0.1m, "wheel_total_loss"),
+            Tuple.Create(0.5m, "wheel_mild_loss"),
+            Tuple.Create(0.2m, "wheel_total_loss"),
+            Tuple.Create(10m, "wheel_great_win"),
+            Tuple.Create(-1m, "wheel_critfail"),
+            Tuple.Create(0.01m, "wheel_total_loss"),
+            Tuple.Create(0.9m, "wheel_break_even"),
+            Tuple.Create(0.3m, "wheel_total_loss"),
+            Tuple.Create(0.7m, "wheel_mild_loss"),
+            Tuple.Create(1.5m, "wheel_mild_win"),
+            Tuple.Create(1.8m, "wheel_mild_win"),
+            Tuple.Create(1m, "wheel_break_even"),
+            Tuple.Create(1.8m, "wheel_mild_win"),
+            Tuple.Create(2m, "wheel_mild_win"),
+            Tuple.Create(1.1m, "wheel_break_even"),
+            Tuple.Create(1.2m, "wheel_break_even")}.ToImmutableArray();
 
+            private readonly decimal _mayhamMultiplier = 10m;
             private readonly ICurrencyService _cs;
             private readonly DbService _db;
             private readonly IImageCache _images;
@@ -53,27 +62,69 @@ namespace NadekoBot.Modules.Gambling
                     return;
                 }
 
-                var result = await _service.WheelOfFortuneSpinAsync(Context.User.Id, amount).ConfigureAwait(false);
+                var _rng = new NadekoRandom();
+                var result = _rng.Next(0, _results.Length);
+                var amount = _results[result].Item1 * amount;
+                string commentary = GetText(_results[result].Item2);
+                var rotation = (result * 360)/_results.Length;
+                var mayham = _rng.Next(0,100);
 
-                using (var bgImage = Image.Load(_images.Roulette))
-                {
-                    bgImage.Mutate(x => x.Rotate(45));
-                    using (var imgStream = bgImage.ToStream())
-                        {
-                            await Context.Channel.SendFileAsync(imgStream, 
-                                                                "result.png", 
-                                                                $@"{Context.User.ToString()} won: {result.Amount + Bc.BotConfig.CurrencySign}").ConfigureAwait(false);
-                        }
+                if(mayham == 0) {
+                    await Context.Channel.SendConfirmAsync(Format.Bold("Let's spin the Wheel of Mayham for x"+_mayhamMultiplier+" multiplier, "+context.User.Mention+"!")).ConfigureAwait(false);
+                    System.Threading.Thread.Sleep(500);
+                    await Context.Channel.SendConfirmAsync(Format.Bold("Ratatata-ta-ta-ta ta ta...!")).ConfigureAwait(false);
+                    System.Threading.Thread.Sleep(500);
+                    await Context.Channel.SendConfirmAsync(Format.Bold("Ta...!")).ConfigureAwait(false);
+                    System.Threading.Thread.Sleep(1000);
+                    await Context.Channel.SendConfirmAsync(Format.Bold("...")).ConfigureAwait(false);
+                    System.Threading.Thread.Sleep(2000);
+                    await Context.Channel.SendConfirmAsync(Format.Bold("Ta!")).ConfigureAwait(false);
+                    System.Threading.Thread.Sleep(200);
+                    amount = amount * _mayhamMultiplier;
                 }
                 
-                await Context.Channel.SendConfirmAsync(
-Format.Bold($@"{Context.User.ToString()} won: {result.Amount + Bc.BotConfig.CurrencySign}
-
-   『{Wof.Multipliers[1]}』   『{Wof.Multipliers[0]}』   『{Wof.Multipliers[7]}』
-
-『{Wof.Multipliers[2]}』      {_emojis[result.Index]}      『{Wof.Multipliers[6]}』
-
-     『{Wof.Multipliers[3]}』   『{Wof.Multipliers[4]}』   『{Wof.Multipliers[5]}』")).ConfigureAwait(false);
+                if(amount > 0) {
+                    await _cs.AddAsync(_userId, "Wheel Of Fortune - won", amount, gamble: true)
+                             .ConfigureAwait(false);
+                } 
+                else if (amount < 0) {
+                    await _cs.RemoveAsync(_userId, "Wheel Of Fortune - lost", -amount, gamble: true)
+                             .ConfigureAwait(false);
+                }
+                
+                using (var bgImage = Image.Load(_images.Roulette))
+                {   
+                    var originalWidth = bgImage.Width;
+                    var originalHeight = bgImage.Height;
+                    
+                    bgImage.Mutate(x => x.Rotate(rotation));
+                    
+                    var widthCrop = (bgImage.Width - originalWidth)/2;
+                    var heightCrop = (bgImage.Height - originalHeight)/2;
+                        
+                    bgImage.Mutate(x => x.Crop(new Rectangle(widthCrop, heightCrop, bgImage.Width - widthCrop, bgImage.Height - heightCrop)));
+                    
+                    using (var ptImage = Image.Load(_images.RoulettePointer)) {
+                        var pointerPosX = bgImage.Width - ptImage.Width;
+                        var pointerPosY = (bgImage.Height / 2) - (ptImage.Height / 2);
+                        bgImage.Mutate(x => x.DrawImage(GraphicsOptions.Default, ptImage, new Point(pointerPosX, pointerPosY)));
+                    }
+                                               
+                    using (var imgStream = bgImage.ToStream())
+                    {
+                        if(amount >= 0) {
+                            await Context.Channel.SendFileAsync(imgStream, 
+                                                                "result.png", 
+                                                                $@"{Context.User.ToString()} won: {amount + Bc.BotConfig.CurrencySign}\n{commentary}")
+                                                 .ConfigureAwait(false);
+                        } else {
+                            await Context.Channel.SendFileAsync(imgStream, 
+                                                                "result.png", 
+                                                                $@"{Context.User.ToString()} lost: {-amount + Bc.BotConfig.CurrencySign}\n{commentary}")
+                                                 .ConfigureAwait(false);
+                        }
+                    }
+                }
             }
         }
     }
