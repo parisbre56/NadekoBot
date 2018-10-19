38633cb72741bc0cb0c735972b4c8231d6d513a7
parisbre56 <parisbre56@users.noreply.github.com>
2018-10-19 19:50:14 +0300

    also get global when fetching locals

--- NadekoBot.Core/Modules/CustomReactions/Services/CustomReactionsService.cs
+++ NadekoBot.Core/Modules/CustomReactions/Services/CustomReactionsService.cs
@@ -413,7 +413,10 @@ namespace NadekoBot.Modules.CustomReactions.Services
             if (guildId == null)
                 return _globalReactions.Values;
             else
-                return _guildReactions.GetOrAdd(guildId.Value, new ConcurrentDictionary<int, CustomReaction>()).Values;
+                var t_gr = _guildReactions.GetOrAdd(guildId.Value, new ConcurrentDictionary<int, CustomReaction>()).Values;
+                var ret = new List<CustomReaction>(t_gr);
+                ret.AddRange(_globalReactions.Values);
+                return ret;
         }
 
         public CustomReaction GetCustomReaction(ulong? guildId, int id)
